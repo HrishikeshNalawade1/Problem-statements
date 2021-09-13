84b5a0742bd207cf2c81c0df948001616f6596d1
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2021-09-13 23:03:42 +0530

    Add files via upload
    	Accept a String from the user and show occurrence of a particular word. (category - Easy).
    
    You need to just accept String  and word to count occurrence from the user as an input. Program should show occurrence of the count of that word.
    
    Sample Input1 :
    How do you do? (indicates accepted String)
    do (indicates accepted word)
    
    Sample ouput1:
    Occurance of Word : 2
    
    Sample Input2 :
    How are you ? (indicates accepted String)
    do (indicates accepted word)
    
    Sample ouput2:
    Occurrence of Word : 0

---
+++ Program2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace program2
+{
+    class Program2
+    {
+        static int countOccurrences(string str,
+                           string word)
+        {
+            // split the string by spaces
+            string[] a = str.Split(' ');
+
+            // search for pattern in string
+            int count = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+
+                // if match found increase count
+                if (word.Equals(a[i]))
+                    count++;
+            }
+
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            string str = "How do you do ";
+            string word = "do";
+            Console.Write(countOccurrences(str, word));
+
+        }
+    }
+}
